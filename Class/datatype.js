62f4db02f499086f2a7634197a0b3bfa728a90ef
Navridhi <navridhi2080.be23@chitkara.edu.in>
2024-09-01 14:53:24 +0530

    updated

---
+++ Class/datatype.js
@@ -0,0 +1,21 @@
+let a=10;
+let b=5;
+let c=2.5;
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+
+let name = "Navridhi";
+console.log(typeof(name))
+
+let x=true;
+console.log(typeof(x))
+
+let y;
+console.log(typeof(y))
+y=26;
+console.log(y)
+console.log(typeof(y))
+
+let j=null;
+console.log(typeof(j))
---
+++ Class/variables.js
@@ -0,0 +1,16 @@
+//var a=10;
+//console.log(a)
+//var a=20;
+//console.log(a)
+
+//let a=10;
+//console.log(a)
+//let a=20;
+//console.log(a)
+
+const a=10;
+console.log(a)
+a=20;
+console.log(a)
+
+
